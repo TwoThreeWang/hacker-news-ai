b7f981d71b8884b47328f4900134c51f77b0ed3e
WangTwoThree <twothreewang@gmail.com>
2025-02-28 13:16:18 +0800

    init

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo '开始拉取最新代码'
+git pull origin main
+echo '打包镜像'
+docker build -t hacker-news-ai:latest .
+echo '启动容器'
+docker-compose up -d --remove-orphans
+echo '清理不再使用的镜像、容器和数据卷'
+docker system prune --all --force --volumes
