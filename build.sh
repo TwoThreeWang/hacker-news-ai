56dfc45f054879e48be378d3f12bc9aeb118bcd6
WangTwoThree <twothreewang@gmail.com>
2025-02-28 14:05:18 +0800

    init

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ docker build -t hacker-news-ai:latest .
 echo '启动容器'
 docker-compose up -d --remove-orphans
 echo '清理不再使用的镜像、容器和数据卷'
-docker system prune --all --force --volumes
+docker system prune --all --force --volumes --filter "label!=keep=true"
