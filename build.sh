3985422edf52839951237c212d0df22540eb5ea9
wangtwothree <twothreewang@gmail.com>
2025-03-02 00:22:18 +0800

    增加 Dev Community 的支持

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ git pull origin main
 echo '打包镜像'
 docker build -t hacker-news-ai:latest .
 echo '启动容器'
-docker-compose up -d --remove-orphans
+docker-compose up --force-recreate -d --remove-orphans
 echo '清理不再使用的镜像、容器和数据卷'
 docker system prune --all --force --volumes --filter "label!=keep=true"
